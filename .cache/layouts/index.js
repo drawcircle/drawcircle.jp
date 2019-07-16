36a01d8bf280fb6043a3f105b73d2d8147eb9cab
Akihiro KATO <4kihiro.kato@gmail.com>
2019-07-16 13:30:44 +0900

    deploy public

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Projects/starter-gatsby-blog/src/layouts/index.js"
+  import data from "/Projects/starter-gatsby-blog/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
