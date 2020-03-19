b80f31f5499b0cc3be194186f907189a2c127e66
Akihiro KATO <4kihiro.kato@gmail.com>
2020-03-19 15:21:15 +0900

    fix error

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/hblkkato/Projects/drawcircle.jp/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/hblkkato/Projects/drawcircle.jp/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/hblkkato/Projects/drawcircle.jp/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/hblkkato/Projects/drawcircle.jp/src/pages/page-2.js")))
